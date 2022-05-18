6d0732bad5755a215637b69cde836d648e5d7579
cent-dxv <centdxv@gmail.com>
2022-05-18 16:40:56 +0300

    bolocking

---
+++ pages/post_blocking/[postId].js
@@ -0,0 +1,63 @@
+
+
+export async function getStaticProps(Context) {
+  const { params } = Context;
+  const responce = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const data = await responce.json();
+  if(!data.id) { return{notFound:true}}
+
+  return {
+    props: {
+      post: data,
+    }
+  }
+}
+
+export async function getStaticPaths(){
+
+    // const responce = await fetch(
+    //     `https://jsonplaceholder.typicode.com/posts`
+    //   );
+    //   const data = await responce.json();
+
+    // const paths = data.map ((post)=>{
+    //     return {
+    //         params :{
+    //             postId: `${post.id}`
+    //          }
+    //     }
+    // })
+
+    return {
+        paths :[
+            {
+                params :{postId: '1'}
+            },
+            {
+                params :{postId: '2'}
+            },{
+                params :{postId: '3'}
+            }
+
+        ],
+        //   paths,
+        // fallback : false,
+        fallback :'blocking'
+    }
+
+}
+
+const PostDetails = ({ post }) => {
+ 
+  return (
+    <div>
+      <h1>post details</h1>
+      <h3> {post.title}</h3>
+      <p> {post.body}</p>
+    </div>
+  );
+};
+
+export default PostDetails;
