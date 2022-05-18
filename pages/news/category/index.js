d40bff926790338d645efa6429b2366f064873c7
cent-dxv <centdxv@gmail.com>
2022-05-18 16:26:53 +0300

    SSR

---
+++ pages/news/category/index.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:4000/news");
+  const data = await response.json();
+  return {
+    props: {
+      news: data,
+    },
+  };
+}
+
+const News = ({ news }) => {
+  return (
+    <div>
+      <h1>CNBC News letest </h1>
+      {news.map((news, index) => (
+        <div key={index}>
+          <Link href={`../news/${news.id}`}>
+            <a>
+              <h2> {news.title}</h2>
+              <h6>{news.description}</h6>{" "}
+              <Link href={`category/${news.category}`}>
+                <a>
+                  {" "}
+                  <h5> {news.category}</h5>{" "}
+                </a>
+              </Link>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default News;
