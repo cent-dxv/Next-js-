bc0faf7a62226466fb4d09ab1cffc5e02f00ad2c
cent-dxv <centdxv@gmail.com>
2022-05-19 10:41:17 +0300

    swr

--- pages/dashboard-swr.js
+++ pages/dashboard-swr.js
@@ -8,16 +8,18 @@ const data = await response.json()
 return data
 }
 const Dashboardswr = () => {
-   const {data ,error} = useSWR('dashboard',fetcher)
+   const {data ,error ,isLoading} = useSWR('dashboard',fetcher)
 
     return (
+
         <div>
+        {isLoading && (<>loadding ...</>)}
           <h2>Dashboard</h2>
-          {console.log(data)}
-          <h2>Posts - {data.posts}</h2>
-          <h2>Likes - {data.likes}</h2>
-          <h2>Followers - {data.followers}</h2>
-          <h2>Following - {data.following}</h2>
+          {console.log(isLoading)}
+          <h2>Posts - {data?.posts}</h2>
+          <h2>Likes - {data?.likes}</h2>
+          <h2>Followers - {data?.followers}</h2>
+          <h2>Following - {data?.following}</h2>
         </div>
       )
 }
